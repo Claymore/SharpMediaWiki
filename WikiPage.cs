ded854ecd44a6d9c88fe92286786c5647b3ac66b
Alexey Bobyakov <claymore.ws@gmail.com>
2009-05-12 18:02:50 +0400

    Added WikiPage and WikiPageSection classes, usefull for parsing a wiki text.

---
+++ WikiPage.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Claymore.SharpMediaWiki
+{
+    public class WikiPage
+    {
+        private string _title;
+        private List<WikiPageSection> _sections;
+        private string _text;
+
+        public WikiPage(string title)
+        {
+            _title = title;
+            _sections = new List<WikiPageSection>();
+        }
+
+        public IList<WikiPageSection> Sections
+        {
+            get { return _sections; }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return _text + string.Join("", _sections.ConvertAll(s => s.Text).ToArray());
+            }
+        }
+
+        public static WikiPage Parse(string title, string text)
+        {
+            WikiPage page = new WikiPage(title);
+
+            string[] lines = text.Split(new char[] { '\n' });
+            StringBuilder sectionText = new StringBuilder();
+            Regex sectionRE = new Regex(@"^(={2,6})([^=].*?)(={2,6})\s*$");
+            int level = 0;
+            string sectionTitle = "";
+            bool found = false;
+            foreach (string line in lines)
+            {
+                Match m = sectionRE.Match(line);
+                if (m.Success)
+                {
+                    if (found)
+                    {
+                        int index = page._sections.Count - 1;
+                        if (index >= 0 &&
+                            index < page._sections.Count &&
+                            page._sections[index].Level < level)
+                        {
+                            page._sections[index].AddSubsection(new WikiPageSection(sectionTitle, level, sectionText.ToString()));
+                        }
+                        else
+                        {
+                            page._sections.Add(new WikiPageSection(sectionTitle, level, sectionText.ToString()));
+                        }
+                        sectionText = new StringBuilder();
+                        found = false;
+                    }
+                    else
+                    {
+                        page._text = sectionText.ToString();
+                        sectionText = new StringBuilder();
+                    }
+                    found = true;
+                    level = Math.Min(m.Groups[1].Length, m.Groups[3].Length);
+                    sectionTitle = m.Groups[2].Value;
+                }
+                else
+                {
+                    sectionText.Append(line + "\n");
+                }
+            }
+            if (found)
+            {
+                int index = page._sections.Count - 1;
+                if (index >= 0 &&
+                    index < page._sections.Count &&
+                    page._sections[index].Level < level)
+                {
+                    page._sections[index].AddSubsection(new WikiPageSection(sectionTitle, level, sectionText.ToString()));
+                }
+                else
+                {
+                    page._sections.Add(new WikiPageSection(sectionTitle, level, sectionText.ToString()));
+                }
+            }
+            else
+            {
+                page._text = sectionText.ToString();
+            }
+            return page;
+        }
+    }
+}
---
+++ WikiPageSection.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+
+namespace Claymore.SharpMediaWiki
+{
+    public class WikiPageSection
+    {
+        private string _title;
+        private int _level;
+        private string _text;
+        private readonly List<WikiPageSection> _subSections;
+
+        public WikiPageSection(string title, int level, string text)
+        {
+            _title = title;
+            _level = level;
+            _text = text;
+            _subSections = new List<WikiPageSection>();
+        }
+
+        public int Level
+        {
+            get { return _level; }
+        }
+
+        public override string ToString()
+        {
+            return _title.Trim();
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public IList<WikiPageSection> Subsections
+        {
+            get { return _subSections; }
+        }
+
+        public string Text
+        {
+            get
+            {
+                string filler = "";
+                for (int i = 0; i < _level; ++i)
+                {
+                    filler += "=";
+                }
+                return string.Format("{0}{1}{0}\n{2}{3}",
+                    filler,
+                    _title,
+                    _text,
+                    string.Join("", _subSections.ConvertAll(s => s.Text).ToArray()));
+            }
+        }
+
+        public void AddSubsection(WikiPageSection section)
+        {
+            int index = _subSections.Count - 1;
+            if (index >= 0 && index < _subSections.Count &&
+                _subSections[index].Level < section.Level)
+            {
+                _subSections[index].AddSubsection(section);
+            }
+            else
+            {
+                _subSections.Add(section);
+            }
+        }
+
+        public void ForEach(Action action)
+        {
+            foreach (WikiPageSection section in Subsections)
+            {
+                action(section);
+            }
+        }
+
+        public T Reduce<T>(T aggregator, ReduceAction<T> action)
+        {
+            T result = aggregator;
+            foreach (WikiPageSection section in Subsections)
+            {
+                result = action(section, result);
+            }
+            return result;
+        }
+
+        public delegate T ReduceAction<T>(WikiPageSection section, T aggregator);
+        public delegate void Action(WikiPageSection section);
+    }
+}
